abed8902f493e981711a6d2cdb29827997f23935
gimsara <gimsarakulathunga2@gmail.com>
2020-05-15 03:44:57 +0530

    Stream,Subject and medium pages created

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ import play from "./components/quiz/play";
 import QuizSummery from "./components/quiz/QuizSummery";
 import SignInForm from "./components/LoginForum/SignInForm";
 import SignUpForm from "./components/LoginForum/SignUpForm";
-
+import Leaderboard from "./components/quiz/Leaderboard";
+import FirstSelection from "./components/Stream/FirstSelection";
+import ScienceStream from "./components/Stream/ScienceStream";
+import Select from "./components/Medium/Select";
+import CommerceStream from "./components/Stream/CommerceStream";
+import ArtStream from "./components/Stream/ArtStream";
+import QuizInstructions_e from "./components/quiz/QuizInsructions_e";
 
 function App() {
   return (
@@ -16,9 +22,16 @@ function App() {
         <Route path="/sign-in" exact component={SignInForm} />
         <Route path="/sign-up" exact component={SignUpForm} />
         <Route path="/play/home" exact component={Home} />
+        <Route path="/play/stream" exact component={FirstSelection} />
+        <Route path="/play/science" exact component={ScienceStream} />
+        <Route path="/play/commerce" exact component={CommerceStream} />
+        <Route path="/play/art" exact component={ArtStream} />
+        <Route path="/play/medium" exact component={Select} />
         <Route path="/play/instructions" exact component={QuizInstructions} />
+        <Route path="/play/instructions_e" exact component={QuizInstructions_e} />
         <Route path="/play/Quiz" exact component={play} />
         <Route path="/play/quizSummery" exact component={QuizSummery} />
+        <Route path="/play/quiz/leaderboard" exact component={Leaderboard} />
 
 
 
